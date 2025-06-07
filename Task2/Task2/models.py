f89dd875376b52e7258fb915ad7ca3327205073f
Aryannp <mraryan605@gmail.com>
2025-06-07 22:46:47 +0545

    fixed required field

--- Task2/Task2/models.py
+++ Task2/Task2/models.py
@@ -3,7 +3,10 @@ from django.db import models
 # todo list model
 class TodoModel(models.Model):
     # setup model's field 
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=100,null=False,blank=False)
     description = models.CharField(max_length=1000)
     completed = models.BooleanField(default=False)
     # owner= models.ForeignKey()
+    
+    def __str__(self):
+        return self.title
