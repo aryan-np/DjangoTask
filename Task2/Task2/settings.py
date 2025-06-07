817e47ae312ce69514afd1a1c700fa47acce5171
Aryannp <mraryan605@gmail.com>
2025-06-07 22:48:43 +0545

    minor changes

--- Task2/Task2/settings.py
+++ Task2/Task2/settings.py
@@ -121,3 +121,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# for rendering json data in the web browser
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ]
+}
