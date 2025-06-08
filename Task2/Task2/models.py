4de4a6c6be6c563de5215128ae99535182b4524d
Aryannp <mraryan605@gmail.com>
2025-06-08 20:19:53 +0545

    add owner field in todo model

--- Task2/Task2/models.py
+++ Task2/Task2/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 # todo list model
 class TodoModel(models.Model):
     # setup model's field 
     title = models.CharField(max_length=100,null=False,blank=False)
     description = models.CharField(max_length=1000)
     completed = models.BooleanField(default=False)
-    # owner= models.ForeignKey()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todos',null=True,blank=True)
+
     
     def __str__(self):
         return self.title
