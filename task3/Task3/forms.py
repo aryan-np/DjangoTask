c704e065fa40c8e005b90140770e3e9027f2b13f
Aryannp <mraryan605@gmail.com>
2025-06-09 22:43:56 +0545

    create profile details form

--- task3/Task3/forms.py
+++ task3/Task3/forms.py
@@ -3,23 +3,31 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
 
+from .models import Profile
 
+# registration form 
 class RegisterForm(UserCreationForm):
     username = forms.CharField(max_length=20, help_text='', label='Username')
     email = forms.EmailField(help_text='', label='Email')
 
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
-        help_texts = {
-            'username': 'Enter a username for you',
-            'password1': '',
-            'password2': '',
-        }
-        
+        fields = ['username', 'email', 'password1', 'password2']  # Which fields to include
 
+
+# Login Form
 class LoginForm(AuthenticationForm):
     username=forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput, label='Password')
     
 
+# Profile Details Displaying and editing form
+class ProfileDetailsForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio', 'profile_picture']
+        widgets = {
+            'bio': forms.Textarea(attrs={'rows':4, 'cols':40}),
+            'profile_picture': forms.TextInput(attrs={'placeholder': 'URL of your profile picture'}),
+        }
